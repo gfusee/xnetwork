88d55c88cc222533331612456067d21cc7ee936d
Fusee <quentus123@orange.fr>
2023-03-10 10:25:55 +0100

    changed api version

--- cli/src/utils/healthchecks/waitForVMQueryToBeReady.ts
+++ cli/src/utils/healthchecks/waitForVMQueryToBeReady.ts
@@ -1,18 +1,27 @@
 import axios from "axios"
 
 export async function waitForVMQueryToBeReady() {
-    const url = "http://localhost:7950/vm-values/query"
-    const payload = {
+    const queryUrl = "http://localhost:7950/vm-values/query"
+    const queryPayload1 = {
         args: [],
         caller: "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqplllst77y4l",
         funcName: "getQueueRegisterNonceAndRewardAddress",
         scAddress: "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqllls0lczs7"
     }
+    const queryPayload2 = {
+        scAddress: "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqylllslmq6y6",
+        funcName: "getAllContractAddresses",
+        args: []
+    }
+
+    const economicsUrl = "http://localhost:7950/network/economics"
 
     while (true) {
         try {
-            const response = await axios.post(url, payload, { validateStatus: () => true })
-            if (response.status === 200) {
+            const queryResponse1 = await axios.post(queryUrl, queryPayload1, { validateStatus: () => true })
+            const queryResponse2 = await axios.post(queryUrl, queryPayload2, { validateStatus: () => true })
+            const economicsResponse = await axios.get(economicsUrl, { validateStatus: () => true })
+            if (queryResponse1.status === 200 && queryResponse2.status === 200 && economicsResponse.status === 200) {
                 break
             }
         } catch (error) {
--- containers/api/wait-for-vm-query-ready.py
+++ containers/api/wait-for-vm-query-ready.py
@@ -2,15 +2,21 @@ import requests
 import time
 
 def wait_for_vm_query_ready():
-    url = "http://testnet:7950/vm-values/query"
-    payload = '{"args":[],"caller":"erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqplllst77y4l","funcName":"getQueueRegisterNonceAndRewardAddress","scAddress":"erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqllls0lczs7"}'
+    query_url = "http://testnet:7950/vm-values/query"
+    query_payload1 = '{"args":[],"caller":"erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqplllst77y4l","funcName":"getQueueRegisterNonceAndRewardAddress","scAddress":"erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqllls0lczs7"}'
+    query_payload2 = '{"scAddress":"erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqylllslmq6y6","funcName":"getAllContractAddresses","args":[]}'
+
+    economics_url = "http://testnet:7950/network/economics"
+
     headers = {
         'Content-Type': 'application/json'
     }
 
     while True:
-        response = requests.request("POST", url, headers=headers, data=payload)
-        if response.status_code == 200:
+        query_response1 = requests.request("POST", query_url, headers=headers, data=query_payload1)
+        query_response2 = requests.request("POST", query_url, headers=headers, data=query_payload2)
+        economics_response = requests.request("GET", economics_url, headers=headers)
+        if query_response1.status_code == 200 and query_response2.status_code == 200 and economics_response.status_code == 200:
             break
         time.sleep(1)
 
